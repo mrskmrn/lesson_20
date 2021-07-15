8be1eba89e99f9ed68fad0e5d287ad4063cc3f5d
marinaRomanchuk <marina.romanchuk23@gmail.com>
2021-07-15 20:25:57 +0300

    Script for greeting

---
+++ app.py
@@ -0,0 +1,6 @@
+import sys
+
+
+def start() -> None:
+	# Prints greeting to the first parametr from the console
+	print(f"Good evening, {sys.argv[1]}, nice to see you!")
---
+++ main.py
@@ -0,0 +1,7 @@
+import app
+
+
+# print(sys.argv)
+
+if __name__ == "__main__":
+	app.start()
